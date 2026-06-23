96d666b2bfdf9176acb8421485a185593e28fab1
yusufcanavariXD <yusuf.arac.876@gmail.com>
2026-06-23 14:15:37 +0300

    ToSmain.cpp
    ToS Trigger

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,53 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+bool g_tosActive = false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+class ToSPopup : public FLAlertLayer {
+public:
+    static ToSPopup* create() {
+        auto ret = new ToSPopup();
+        if (ret && ret->init(260.0f, 320.0f, "LEGAL NOTICE", "Accept")) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    bool init(float width, float height, char const* title, char const* btnText) {
+        if (!FLAlertLayer::init(nullptr, title, "By continuing past this point, you agree to hand over all your icons, mana orbs, and stars to YuSoft. Please read carefully.", btnText, nullptr, width)) {
+            return false;
+        }
+        return true;
+    }
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    void onBtn1(CCObject* sender) override {
+        FLAlertLayer::onBtn1(sender);
+        g_tosActive = false;
+        if (auto playLayer = PlayLayer::get()) {
+            playLayer->startMusic();
+        }
+    }
+};
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+class $modify(MyPlayLayer, PlayLayer) {
+    void update(float dt) {
+        if (g_tosActive) {
+            if (this->m_player1) {
+                this->m_player1->m_positionX = this->m_player1->m_positionX;
+            }
+            return;
+        }
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        PlayLayer::update(dt);
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        if (this->m_time > 2.0f && !g_tosActive && !this->m_isDead) {
+            g_tosActive = true;
+            this->m_player1->m_platformerXVelocity = 0;
+            ToSPopup::create()->show();
+        }
+    }
 };
